282c25088d0dec8ea44a9cbf96876f0efbdb2139
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2023-12-10 22:06:51 +0530

    Update ami.ps1

--- ami.ps1
+++ ami.ps1
@@ -67,7 +67,7 @@ if ($amiStatus -eq "available") {
 
     # Slack API endpoint and message payload
     $uri = "https://slack.com/api/chat.postMessage"
-    $token = "xoxb-6304431362048-6307460347623-YdQCdQw2v9np53kM2PKMj40u"  # Replace with your Slack API token
+    $token = "xoxb-6304431362048-6307460347623-iixrfzt1YMJdh8fkzn5ylv76"  # Replace with your Slack API token
     $headers = @{
         "Authorization" = "Bearer $token"
     }
